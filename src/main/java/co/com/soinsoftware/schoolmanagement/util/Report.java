ff31674906c4e37a2c80bca51d703b63a5bc1627
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2016-06-15 15:10:35 -0500

    New report generation to improve performance

--- src/main/java/co/com/soinsoftware/schoolmanagement/util/Report.java
+++ src/main/java/co/com/soinsoftware/schoolmanagement/util/Report.java
@@ -1,33 +1,22 @@
 package co.com.soinsoftware.schoolmanagement.util;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.cmp;
-import static net.sf.dynamicreports.report.builder.DynamicReports.col;
-import static net.sf.dynamicreports.report.builder.DynamicReports.sbt;
-import static net.sf.dynamicreports.report.builder.DynamicReports.type;
-
-import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
-import net.sf.dynamicreports.report.builder.DynamicReports;
-import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
-import net.sf.dynamicreports.report.builder.component.TextFieldBuilder;
-import net.sf.dynamicreports.report.builder.style.StyleBuilder;
-import net.sf.dynamicreports.report.builder.style.StyleBuilders;
-import net.sf.dynamicreports.report.builder.subtotal.AggregationSubtotalBuilder;
-import net.sf.dynamicreports.report.constant.HorizontalImageAlignment;
-import net.sf.dynamicreports.report.constant.HorizontalTextAlignment;
-import net.sf.dynamicreports.report.constant.VerticalImageAlignment;
-import net.sf.dynamicreports.report.constant.VerticalTextAlignment;
-import net.sf.dynamicreports.report.exception.DRException;
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,7 +40,16 @@ public class Report {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Report.class);
 
-	private final JasperReportBuilder report;
+	private static final String REPORT_NAME = "/school.jasper";
+
+	private static final String PARAM_SCHOOL_IMAGE = "SchoolImg";
+	private static final String PARAM_COMPANY_NAME = "CompanyName";
+	private static final String PARAM_COMPANY_ID = "CompanyId";
+	private static final String PARAM_CLASS_AND_USER = "ClassAndUser";
+	private static final String PARAM_PERIOD = "Period";
+	private static final String PARAM_DOCUMENT_NUMBER = "DocumentNumber";
+	private static final String PARAM_GUARDIAN_NAME = "GuardianName";
+	private static final String PARAM_GROUP_DIRECTOR = "GroupDirector";
 
 	private final ClassRoomBO classRoom;
 
@@ -72,150 +70,54 @@ public class Report {
 		this.classRoom = classRoom;
 		this.student = student;
 		this.finalNoteSet = finalNoteSet;
-		report = DynamicReports.report();
 	}
 
 	public boolean generate() {
 		boolean generated = false;
 		try {
-			this.buildTitleSection();
-			this.buildPageHeaderSection();
-			this.buildDetailSection();
-			this.buildColumnFooter();
-			report.toPdf(new FileOutputStream(this.getReportFile()));
+			System.out.println("Loading jasper file");
+			final JasperReport jasReport = this.loadJasperReport();
+			final Map<String, Object> parameters = this.createParameters();
+			final JRDataSource dataSource = this.createDataSource();
+			System.out.println("Filling report");
+			final JasperPrint jasperPrint = JasperFillManager.fillReport(
+					jasReport, parameters, dataSource);
+			System.out.println("Starting show");
+			JasperExportManager.exportReportToPdfFile(jasperPrint, this
+					.getReportFile().getAbsolutePath());
 			generated = true;
-		} catch (DRException | FileNotFoundException ex) {
+		} catch (JRException | FileNotFoundException ex) {
 			LOGGER.error(ex.getMessage(), ex);
 		}
 		return generated;
 	}
 
-	private void buildTitleSection() throws FileNotFoundException {
+	private JasperReport loadJasperReport() throws JRException {
+		final InputStream resourceIS = this.getClass().getResourceAsStream(
+				REPORT_NAME);
+		return (JasperReport) JRLoader.loadObject(resourceIS);
+	}
+
+	private Map<String, Object> createParameters() throws FileNotFoundException {
 		final File imgFile = new File(this.school.getPhoto());
 		final InputStream image = new FileInputStream(imgFile);
-		final String schoolName = this.school.getName();
 		final StringBuilder schoolId = new StringBuilder("NIT: ");
 		schoolId.append(this.school.getNit());
 		schoolId.append(" ");
 		schoolId.append("DANE: ");
 		schoolId.append(this.school.getDane());
-		final StyleBuilder styleFont15 = this
-				.getNewStyle()
-				.setFontSize(15)
-				.setLeftPadding(5)
-				.bold()
-				.setTextAlignment(HorizontalTextAlignment.LEFT,
-						VerticalTextAlignment.BOTTOM);
-		final StyleBuilder styleFont6 = this
-				.getNewStyle()
-				.setFontSize(6)
-				.setLeftPadding(5)
-				.bold()
-				.setTextAlignment(HorizontalTextAlignment.LEFT,
-						VerticalTextAlignment.TOP);
-		this.report.title(
-				cmp.horizontalList().add(
-						cmp.image(image).setStyle(
-								this.getNewStyle().setImageAlignment(
-										HorizontalImageAlignment.RIGHT,
-										VerticalImageAlignment.BOTTOM)),
-						cmp.verticalList().add(
-								this.getTextField(schoolName, styleFont15),
-								this.getTextField(schoolId.toString(),
-										styleFont6).setFixedHeight(45))),
-				cmp.verticalGap(30));
-	}
-
-	private void buildPageHeaderSection() {
-		final StyleBuilder styleFont8 = this.getNewStyle().setFontSize(8)
-				.setBackgroundColor(Color.WHITE)
-				.setBorder(DynamicReports.stl.pen1Point()).setLeftPadding(5);
-		final StyleBuilder styleFont12 = this.getNewStyle().setFontSize(12)
-				.bold().setBackgroundColor(Color.LIGHT_GRAY)
-				.setBorder(DynamicReports.stl.pen1Point()).setLeftPadding(5);
-		this.report.pageHeader(cmp.verticalList().add(
-				this.getTextField(this.getPageHeaderTitle(), styleFont12),
-				cmp.horizontalList(this.getTextField("Periodo:", styleFont8)
-						.setFixedDimension(100, 15), this.getTextField(
-						this.getPeriodInformation(), styleFont8)),
-				cmp.horizontalList(
-						this.getTextField("Núm. Documento:", styleFont8)
-								.setFixedDimension(100, 15), this.getTextField(
-								this.student.getDocumentNumber(), styleFont8)),
-				cmp.horizontalList(this.getTextField("Acudiente:", styleFont8)
-						.setFixedDimension(100, 15), this.getTextField(
-						this.getFullName(this.student.getGuardian1()),
-						styleFont8)), cmp.verticalGap(20)));
-	}
-
-	private void buildDetailSection() {
-		final StyleBuilder styleRowTitle = this.getNewStyle().bold()
-				.setBackgroundColor(Color.LIGHT_GRAY)
-				.setBorder(DynamicReports.stl.pen1Point()).setLeftPadding(5);
-		final StyleBuilder styleRowDetail = this
-				.getNewStyle()
-				.setBorder(DynamicReports.stl.pen1Point())
-				.setFontSize(8)
-				.setLeftPadding(5)
-				.setTextAlignment(HorizontalTextAlignment.JUSTIFIED,
-						VerticalTextAlignment.TOP);
-		final StyleBuilder styleRowBold = this
-				.getNewStyle()
-				.bold()
-				.setBackgroundColor(Color.LIGHT_GRAY)
-				.setBorder(DynamicReports.stl.pen1Point())
-				.setLeftPadding(5)
-				.setTextAlignment(HorizontalTextAlignment.JUSTIFIED,
-						VerticalTextAlignment.TOP);
-		final TextColumnBuilder<BigDecimal> colNoteValue = col.column(
-				"Calificación", "finalNote.value", type.bigDecimalType());
-		final TextColumnBuilder<String> colClass = col.column("Asignatura",
-				"name", type.stringType());
-		final TextColumnBuilder<String> colQualitative = col.column(
-				"Cualitativa", "qualitativeNote", type.stringType());
-		final TextColumnBuilder<String> colAchievement = col.column("Logros",
-				"achievements", type.stringType());
-		AggregationSubtotalBuilder<Number> sumNoteValue = sbt.avg(colNoteValue)
-				.setStyle(styleRowBold);
-		AggregationSubtotalBuilder<String> sumClass = sbt.text(
-				"Promedio del estudiante", colClass).setStyle(styleRowBold);
-		AggregationSubtotalBuilder<String> sumQualitative = sbt.text("",
-				colQualitative).setStyle(styleRowBold);
-		AggregationSubtotalBuilder<String> sumAchievement = sbt.text("",
-				colAchievement).setStyle(styleRowBold);
-		this.report
-				.columns(colClass.setFixedWidth(125),
-						colNoteValue.setFixedWidth(65),
-						colQualitative.setFixedWidth(100), colAchievement)
-				.setColumnTitleStyle(styleRowTitle)
-				.setColumnStyle(styleRowDetail)
-				.subtotalsAtSummary(sumNoteValue).subtotalsAtSummary(sumClass)
-				.subtotalsAtSummary(sumQualitative)
-				.subtotalsAtSummary(sumAchievement);
-		this.report.setDataSource(this.createDataSource());
-
-	}
-
-	private void buildColumnFooter() {
-		final StyleBuilder styleFont12 = this
-				.getNewStyle()
-				.setFontSize(12)
-				.bold()
-				.setLeftPadding(5)
-				.setTextAlignment(HorizontalTextAlignment.LEFT,
-						VerticalTextAlignment.TOP);
 		final UserBO teacher = this.classRoom.getTeacher();
-		this.report.columnFooter(cmp.verticalList().add(
-				cmp.line().setPen(DynamicReports.stl.pen1Point())
-						.setFixedWidth(200),
-				this.getTextField(this.getFullName(teacher), styleFont12),
-				this.getTextField("Director(a) de grupo", styleFont12),
-				cmp.verticalGap(40)));
-	}
-
-	private TextFieldBuilder<String> getTextField(final String text,
-			final StyleBuilder style) {
-		return cmp.text(text).setStyle(style);
+		final Map<String, Object> parameters = new HashMap<>();
+		parameters.put(PARAM_SCHOOL_IMAGE, image);
+		parameters.put(PARAM_COMPANY_NAME, this.school.getName());
+		parameters.put(PARAM_COMPANY_ID, schoolId.toString());
+		parameters.put(PARAM_CLASS_AND_USER, this.getPageHeaderTitle());
+		parameters.put(PARAM_PERIOD, this.getPeriodInformation());
+		parameters.put(PARAM_DOCUMENT_NUMBER, this.student.getDocumentNumber());
+		parameters.put(PARAM_GUARDIAN_NAME,
+				this.getFullName(this.student.getGuardian1()));
+		parameters.put(PARAM_GROUP_DIRECTOR, this.getFullName(teacher));
+		return parameters;
 	}
 
 	private String getPageHeaderTitle() {
@@ -242,10 +144,6 @@ public class Report {
 		guardianInformation.append(" ");
 		guardianInformation.append(user.getLastName());
 		return guardianInformation.toString();
-	}
-
-	private StyleBuilder getNewStyle() {
-		return new StyleBuilders().style();
 	}
 
 	private JRDataSource createDataSource() {
